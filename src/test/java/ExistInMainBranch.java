7b736b5ddf3aa1d9a003105a994757d622757158
Nijat Muhtar <nijatmn@gmail.com>
2022-06-05 12:39:28 -0400

    first backup of the project

---
+++ src/test/java/ExistInMainBranch.java
@@ -0,0 +1,7 @@
+public class ExistInMainBranch {
+
+    public static void main(String[] args) {
+        System.out.println("This class exist in ");
+        System.out.println("main copy of the project");
+    }
+}
